67932669a96970b3056b626140306042eecf4c71
Marco <mgiammer@gmail.com>
2021-03-29 17:32:37 +0200

    ae

--- movement.py
+++ movement.py
@@ -1,7 +1,7 @@
 from gpiozero import DistanceSensor, Robot, DigitalInputDevice
 from time import sleep
 
-sensorFront = DistanceSensor(echo=21, trigger=20, threshold_distance=0.1)
+sensorFront = DistanceSensor(echo="BOARD10", trigger="", threshold_distance=0.1)
 sensorBack = DistanceSensor(echo=18, trigger=17, threshold_distance=0.1)
 robot = Robot(left=(4, 14), right=(17, 18)) #A tuple of two (or three) GPIO pins representing the forward and backward inputs of the left and right motor’s controller
 line_sensor = DigitalInputDevice(9)
