90f021ab492eb7571029dc3cb443fbd7684ac6ed
Marco <mgiammer@gmail.com>
2021-04-11 12:35:37 +0200

    update: eliminated redundant lines

--- movement.py
+++ movement.py
@@ -10,8 +10,6 @@ class Movement:
         self.__robot = Robot(left=(4, 14), right=(17, 18))
         self.__line_sensor = DigitalInputDevice(9)
         self.__line_sensor.when_deactivated = self.__avoid_line
-        self.__sensorFront.threshold_distance = 10
-        self.__sensorBack.threshold_distance = 10
         self.__sensorFront.when_activated = self.__obstacle_front
         self.__sensorBack.when_activated = self.__obstacle_back
         self.__sensorFront.when_deactivated = self.__robot.stop
@@ -21,26 +19,26 @@ class Movement:
     def __avoid_line(self):
         logging.info('Starting line avoidance routine')
         self.__robot.stop()
-        self.__robot.backward(speed=0.5, curve_left=1)
+        self.__robot.backward(speed=0.25, curve_left=1)
         self.__line_sensor.wait_for_active()
         self.__line_sensor.wait_for_inactive()
         self.__robot.stop()
-        self.__robot.forward(speed=0.5)
+        self.__robot.forward(speed=0.25)
         logging.info('Line avoidance finished')
 
     def __obstacle_front(self):
         logging.info('Avoiding obstacle on front')
         self.__robot.stop()
-        self.__robot.backward(speed=0.5)
+        self.__robot.backward(speed=0.25)
 
     def __obstacle_back(self):
         logging.info('Avoiding obstacle on back')
         self.__robot.stop()
-        self.__robot.forward(speed=0.5)
+        self.__robot.forward(speed=0.25)
 
     def move_idle(self):
         self.__robot.stop()
-        self.__robot.forward(speed=0.5)
+        self.__robot.forward(speed=0.25)
 
 
 movement = Movement
