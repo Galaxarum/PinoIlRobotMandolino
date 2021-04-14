82ce46c76b8b692bd5e8fd94ac9304a422fa0b22
matteo <matteo.secco97@gmail.com>
2021-04-14 15:24:23 +0200

    Edited movement to use LineSensor class instead of digitalInputDevice

--- movement.py
+++ movement.py
@@ -1,4 +1,4 @@
-from gpiozero import DistanceSensor, Robot, DigitalInputDevice
+from gpiozero import DistanceSensor, Robot, DigitalInputDevice, LineSensor
 import logging
 
 
@@ -8,19 +8,17 @@ class Movement:
         self.__sensorFront = DistanceSensor(echo=23, trigger=24, threshold_distance=0.1)
         self.__sensorBack = DistanceSensor(echo=2, trigger=3, threshold_distance=0.1)
         self.__robot = Robot(left=(13, 19), right=(5, 6))
-        self.__line_sensor = DigitalInputDevice(4)
-        self.__line_sensor.when_deactivated = self.__avoid_line
+        self.__line_sensor = LineSensor(4)
+        self.__line_sensor.when_line = self.__avoid_line
         self.__sensorFront.when_deactivated = self.__obstacle_front
         self.__sensorBack.when_deactivated = self.__obstacle_back
         logging.info('Robot initialized')
 
     def __avoid_line(self):
         logging.info('Starting line avoidance routine')
-        self.__robot.stop()
         self.__robot.backward(speed=0.25, curve_left=1)
-        self.__line_sensor.wait_for_active()
-        self.__line_sensor.wait_for_inactive()
-        self.__robot.stop()
+        self.__line_sensor.wait_for_no_line()
+        self.__line_sensor.wait_for_line()
         self.__robot.forward(speed=0.25)
         logging.info('Line avoidance finished')
 
