43d1708464b6fbec65ce2d6d5dbec93a7ad54508
Mattia <mattia3.pagani@mail.polimi.it>
2021-04-15 10:19:34 +0200

    add face_detector_listener.py to be inherit for listening face recognition events

---
+++ face_tracking/face_detector_listener.py
@@ -0,0 +1,24 @@
+class FaceDetectorEventListener:
+    CENTER = 'center'
+    RIGHT = 'right'
+    LEFT = 'left'
+
+    def __init__(self):
+        pass
+
+    def on_valid_face_present(self):
+        """
+        Called when a face is being detected for a certain amount of time.
+        """
+        pass
+
+    def on_face_position(self, position):
+        """
+        Called when a face <change> position in the camera window.
+        <change> := if a face stay in a specified part of the screen the method is NOT called. It's called
+        only if the face switch from a section to another.
+
+        Arguments:
+             position: Position of the face in the camera window
+        """
+        pass
