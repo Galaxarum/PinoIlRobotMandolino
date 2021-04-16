bd021dbf780112b7fd4b71209fe265f353c431a4
Galaxar <matteo.secco97@gmail.com>
2021-04-16 16:22:11 +0200

    Fixed line avoidance test

--- line_avoidance_test.py
+++ line_avoidance_test.py
@@ -3,6 +3,5 @@ from movement import Movement
 if __name__ == '__main__':
     m = Movement()
     m.move_idle()
-    while input('Press any key to stop'):
-        pass
+    input('Press any key to stop')
     m.stop()
