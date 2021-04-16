7f2163beaf55cfb3d69baf0951f63871ae5244ae
Galaxar <matteo.secco97@gmail.com>
2021-04-17 00:19:58 +0200

    First attempt on led matrices

---
+++ led_matrices.py
@@ -0,0 +1,10 @@
+from luma.core.interface.serial import spi, noop
+from luma.core.legacy import text
+from luma.core.render import canvas
+from luma.led_matrix.device import max7219
+
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, width=16, height=16)
+
+with canvas(device) as draw:
+    text(draw, (0, 0), 'Hello', fill='white')
