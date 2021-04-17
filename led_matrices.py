8a9895ef7785e091eff743c4ffc790acf19f8945
Galaxar <matteo.secco97@gmail.com>
2021-04-17 13:06:51 +0200

    Attempt to solve flipping, fixed indexes

--- led_matrices.py
+++ led_matrices.py
@@ -7,7 +7,7 @@ from luma.led_matrix.device import max7219
 class EyeLedMatrix(max7219):
     def preprocess(self, image):
         super(EyeLedMatrix, self).preprocess(image)
-        upper = image.crop((0, 0, image.width, image.height/2))  # TODO: get upper half
+        upper = image.crop((0, 0, image.width-1, image.height/2-1))  # TODO: get upper half
         upper = upper.transpose(Image.FLIP_TOP_BOTTOM)
         image.paste(upper)
         return image
