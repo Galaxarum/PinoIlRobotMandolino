4b6377ad2ae90a7bd6dac3873874c88de06013ed
Galaxar <matteo.secco97@gmail.com>
2021-04-17 13:13:42 +0200

    Trying to display text for debugging

--- led_matrices.py
+++ led_matrices.py
@@ -2,7 +2,7 @@ from PIL import Image
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
 from luma.led_matrix.device import max7219
-
+from luma.core.legacy import text
 
 class EyeLedMatrix(max7219):
     def preprocess(self, image):
@@ -18,3 +18,4 @@ device = EyeLedMatrix(serial, width=16, height=16)
 
 with canvas(device) as draw:
     draw.rectangle(device.bounding_box, outline="white", fill="black")
+    text(draw, (2,2), 'Pino', fill='white')
