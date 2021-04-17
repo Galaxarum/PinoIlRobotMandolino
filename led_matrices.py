696d19172fa150c13bae1d46a1e2b176ac9d7aee
Galaxar <matteo.secco97@gmail.com>
2021-04-17 12:47:38 +0200

    2x2 grid

--- led_matrices.py
+++ led_matrices.py
@@ -1,13 +1,16 @@
 from luma.core.interface.serial import spi, noop
-from luma.core.legacy import text
 from luma.core.render import canvas
 from luma.led_matrix.device import max7219
-from PIL import ImageFont
+
+
+class EyeLedMatrix(max7219):
+    def preprocess(self, image):
+        super(EyeLedMatrix, self).preprocess(image)
+        # TODO: rotate line
+
 
 serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial, width=16, height=16)
-
-font = ImageFont.truetype("examples/pixelmix.ttf", 8)
+device = EyeLedMatrix(serial, width=16, height=16)
 
 with canvas(device) as draw:
     draw.rectangle(device.bounding_box, outline="white", fill="black")
