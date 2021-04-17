c1148c85faa441dbd0dab78b6e38c51921eee7b2
Galaxar <matteo.secco97@gmail.com>
2021-04-17 13:04:14 +0200

    Attempt to solve flipping

--- led_matrices.py
+++ led_matrices.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
 from luma.led_matrix.device import max7219
@@ -6,7 +7,10 @@ from luma.led_matrix.device import max7219
 class EyeLedMatrix(max7219):
     def preprocess(self, image):
         super(EyeLedMatrix, self).preprocess(image)
-        # TODO: rotate line
+        upper = image.crop((0, 0, image.width, image.height/2))  # TODO: get upper half
+        upper = upper.transpose(Image.FLIP_TOP_BOTTOM)
+        image.paste(upper)
+        return image
 
 
 serial = spi(port=0, device=0, gpio=noop())
