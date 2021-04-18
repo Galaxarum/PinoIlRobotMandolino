10a76e4f1122efbd6cfc45662b0de6b3cbb5497f
Galaxar <matteo.secco97@gmail.com>
2021-04-18 18:22:41 +0200

    Sketches of eyes in custom class

--- led_matrices.py
+++ led_matrices.py
@@ -4,6 +4,7 @@ from luma.core.render import canvas
 from luma.led_matrix.device import max7219
 from luma.core.legacy import text
 
+
 class EyeLedMatrix(max7219):
     def preprocess(self, image):
         image = super(EyeLedMatrix, self).preprocess(image)
@@ -14,9 +15,52 @@ class EyeLedMatrix(max7219):
         return image
 
 
-serial = spi(port=0, device=0, gpio=noop())
-device = EyeLedMatrix(serial, width=16, height=16)
+class LedMatrices:
+    def __init__(self):
+        serial = spi(port=0, device=0, gpio=noop())
+        self.device = EyeLedMatrix(serial, width=16, height=16)
 
-with canvas(device) as draw:
-    draw.rectangle(device.bounding_box, outline="white", fill="black")
-    text(draw, (2,2), 'Pino', fill='white')
+    def __draw(self, drawing_function):
+        with canvas(self.device) as draw:
+            drawing_function(draw)
+
+    def __eye_debug_drawer(self, draw):
+        draw.rectangle(self.device.bounding_box, outline="white", fill="black")
+        text(draw, (2, 2), 'Pino', fill='white')
+
+    def __eye_neutral_drawer(self, draw):
+        draw.arc(self.device.bounding_box, 0, 360, fill='black')
+        draw.ellipse([(6, 6), (9, 9)], outline='black', fill='black')
+
+    def __eye_angry_drawer(self, draw, angle):
+        self.__eye_neutral_drawer(draw)
+        draw.pieslice([(0, 0), (15, 15)], 180+angle, -angle, fill='black')
+
+    def __eye_flat_drawer(self, draw, angle):
+        self.__eye_neutral_drawer(draw)
+        draw.chord([(0, 0), (15, 15)], 180 + angle, -angle, outline='black', fill='black')
+
+    def __eye_sad_drawer(self, draw, angle):
+        self.__eye_neutral_drawer(draw)
+        draw.chord([(0, 0), (15, 15)], 180, -angle, fill='black')
+        draw.chord([(0, 0), (15, 15)], 180 + angle, 360, fill='black')
+
+    def eye_debug(self):
+        self.__draw(self.__eye_debug_drawer)
+
+    def eye_neutral(self):
+        self.__draw(self.__eye_neutral_drawer)
+
+    def eye_angry(self, angle=20):
+        self.__draw(lambda draw: self.__eye_angry_drawer(draw, angle=angle))
+
+    def eye_suspicious(self, angle=25):
+        self.__draw(lambda draw: self.__eye_flat_drawer(draw, angle=angle))
+
+    def eye_bored(self, angle=10):
+        self.__draw(lambda draw: self.__eye_flat_drawer(draw, angle=angle))
+
+    def eye_sad(self, angle=45):
+        self.__draw(lambda draw: self.__eye_sad_drawer(draw, angle=angle))
+
+
