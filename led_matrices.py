1f7150fe10d776e44154d9bea8ed0269e8d6ef29
Galaxar <matteo.secco97@gmail.com>
2021-04-19 15:14:17 +0200

    Fixed mouth size

--- led_matrices.py
+++ led_matrices.py
@@ -46,7 +46,7 @@ class LedMatrices:
 
         serial1 = spi(port=1, device=0, gpio=noop())
         self.mouth_matrix = max7219(serial1)
-        self.mouth = viewport(self.mouth_matrix, width=32, height=16)
+        self.mouth = viewport(self.mouth_matrix, width=16, height=8)
 
         def scroll_mouth():
             while self.mouth is not None:
