7d09603a78fcc48023dd1e1523a199ce12fcc1e2
Galaxar <matteo.secco97@gmail.com>
2021-04-19 13:01:40 +0200

    Added mouth support

--- led_matrices.py
+++ led_matrices.py
@@ -4,37 +4,75 @@ from PIL import Image
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
 from luma.led_matrix.device import max7219
+from luma.core.virtual import viewport
 from luma.core.legacy import text
+from threading import Thread
+import math
+import atexit
 
 
 class EyeLedMatrix(max7219):
     def preprocess(self, image):
         image = super(EyeLedMatrix, self).preprocess(image)
-        upper = image.crop((0, 0, image.width, image.height/2))  # TODO: get upper half
+        upper = image.crop((0, 0, image.width, image.height/2))
         upper = upper.transpose(Image.FLIP_TOP_BOTTOM)
         upper = upper.transpose(Image.FLIP_LEFT_RIGHT)
         image.paste(upper)
         return image
 
 
+def _plot_sin(draw, size, amplitude=None, steps=None):
+    if amplitude is None:
+        amplitude = size[1]/2-1
+    if steps is None:
+        steps = min(size)
+    prev = (0, size[1]/2)
+    for r in range(int(size[0] / steps)):
+        for i in range(steps):
+            x = 2 * math.pi * (i / steps)
+            y = amplitude * math.sin(x)
+            y = int(y)
+            y += size[1] / 2
+            point = (i + r * steps, y)
+            draw.point(point, fill='white')
+            draw.line([prev, point])
+            prev = point
+
+
 class LedMatrices:
     def __init__(self):
         serial0 = spi(port=0, device=0, gpio=noop())
-        serial1 = spi(port=1, device=0, gpio=noop())
         self.eyes = EyeLedMatrix(serial0, width=16, height=16)
-        self.mouth = max7219(serial1)
+
+        serial1 = spi(port=1, device=0, gpio=noop())
+        self.mouth_matrix = max7219(serial1)
+        self.mouth = viewport(self.mouth_matrix, width=32, height=16)
+
+        def scroll_mouth():
+            while self.mouth is not None:
+                for offset in range(self.mouth.width):
+                    self.mouth.set_position((offset, 0))
+                    sleep(0.1)
+        Thread(target=scroll_mouth).start()
+
+        self.eye_neutral()
         self.stop_speak()
+
+        atexit.register(self.clear)
+
+    def clear(self):
+        self.mouth.clear()
+        self.mouth = None
+        self.mouth_matrix.clear()
+        self.eyes.clear()
 
     def speak(self):
         with canvas(self.mouth) as draw:
-            draw.line([(0, 5), (4, 2)], fill='white', width=1)
-            draw.line([(5, 2), (8, 5)], fill='white', width=1)
-
+            _plot_sin(draw, size=self.mouth.size)
 
     def stop_speak(self):
         with canvas(self.mouth) as draw:
-            y = self.mouth.bounding_box[3]/2
-            draw.line([(0, y), (self.mouth.bounding_box[2], y)], fill='white',width=1)
+            _plot_sin(draw, size=self.mouth.size, amplitude=2)
 
     def __draw(self, drawing_function):
         with canvas(self.eyes) as draw:
