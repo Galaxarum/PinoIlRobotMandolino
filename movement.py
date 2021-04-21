70ead5bf803da80de5e547f6c0c8017733098775
matteo <matteo.secco97@gmail.com>
2021-04-21 16:58:02 +0200

    Managed critical/edge avoidance conflicts

--- movement.py
+++ movement.py
@@ -11,14 +11,8 @@ class Movement(FaceDetectorEventListener):
 
     def __init__(self, standard_speed=0.25, avoidance_speed=0.25):
         self.__sensorFront = DistanceSensor(echo=24, trigger=25, threshold_distance=0.1)
-        self.__sensorFront.when_in_range = self.__obstacle_front
-
         self.__sensorBack = DistanceSensor(echo=27, trigger=22, threshold_distance=0.1)
-        self.__sensorBack.when_in_range = self.__obstacle_back
-
         self.__line_sensor = LineSensor(4, queue_len=10)
-        self.__line_sensor.when_line = self.__avoid_line
-
         self.__robot = Robot(left=(13, 26), right=(5, 6))
 
         self.__standard_speed = standard_speed
@@ -30,63 +24,79 @@ class Movement(FaceDetectorEventListener):
         atexit.register(lambda: self.__line_sensor.close())
 
         self.__log = logging.getLogger('movement')
-        self.__log.info('Movement initialized')
 
+        self.__reset_avoidances()
         self.move_idle()
 
-    def __stop_avoiding(self):
+        self.__log.info('Movement initialized')
+
+    def __reset_avoidances(self):
+        self.__sensorFront.when_in_range = self.__obstacle_front
         self.__sensorFront.when_out_of_range = None
+
+        self.__sensorBack.when_in_range = self.__obstacle_back
         self.__sensorBack.when_out_of_range = None
+
         self.__line_sensor.when_line = self.__avoid_line
         self.__line_sensor.when_no_line = None
 
     def __avoid_line(self):
-        return   # todo: re-enable me
-        print('Starting line avoidance routine')
-        self.__robot.backward(speed=self.__avoidance_speed, curve_left=1)
-        self.__sensorFront.when_in_range = None
-
         def on_leave_triggering_line():
-            sleep(1)
             self.__line_sensor.when_line = on_line_again
             self.__line_sensor.when_no_line = None
-            print('waiting to find line on back')
 
-        def on_line_again():    # todo: critical case of obstacle in front when leaving second line
-            self.__robot.forward(speed=self.__avoidance_speed)
-            self.__sensorFront.when_in_range = self.__obstacle_front
-            self.__sensorBack.when_in_range = None
             sleep(1)
+            self.__log.info('waiting to find line on back')
+
+        def on_line_again():
+            self.__sensorFront.when_in_range = self.stop    # if obstacle prevents from leaving the line, wait
+            self.__sensorFront.when_out_of_range = on_line_again  # resume line routine from here when obstacle removed
+
+            self.__sensorBack.when_in_range = None  # ignore back obstacles when moving forward
+
             self.__line_sensor.when_line = None
             self.__line_sensor.when_no_line = on_leave_ending_line
-            print('waiting to leave line on back')
+
+            self.__robot.forward(speed=self.__avoidance_speed)
+            sleep(1)
+            self.__log.info('waiting to leave line on back')
 
         def on_leave_ending_line():
-            self.__line_sensor.when_line = self.__avoid_line
-            self.__line_sensor.when_no_line = None
-            self.__sensorBack.when_in_range = self.__obstacle_back
-            print('Line avoidance finished')
+            self.__reset_avoidances()
+            self.__log.info('Line avoidance finished')
 
-        # todo: critical case of obstacle on back when leaving first line
+        self.__log.info('Starting line avoidance routine')
+        self.__reset_avoidances()
+
+        self.__sensorFront.when_in_range = None     # ignore front obstacles while moving back
+
         self.__line_sensor.when_line = None
         self.__line_sensor.when_no_line = on_leave_triggering_line
-        print('waiting to leave triggering line')
+
+        self.__sensorBack.when_in_range = self.stop    # if obstacle prevents from leaving the line, wait
+        self.__sensorBack.when_out_of_range = self.__avoid_line  # resume line routine from here when obstacle removed
+
+        self.__robot.backward(speed=self.__avoidance_speed, curve_left=1)
+        self.__log.info('waiting to leave triggering line')
 
     def __obstacle_front(self):
         def on_avoiding_ended():
             self.__sensorFront.when_out_of_range = None
+            self.__line_sensor.when_line = self.__avoid_line
             self.move_idle(2)
-        self.__stop_avoiding()
+
+        self.__reset_avoidances()
         self.__log.info('Avoiding obstacle on front')
         self.__robot.backward(speed=self.__avoidance_speed, curve_left=0.5)
-        # todo: what if i find a line when going back?
+        # if a line is found when going back, stop. out of range will remain active, allowing to restart movement
+        self.__line_sensor.when_line = self.stop
         self.__sensorFront.when_out_of_range = on_avoiding_ended
 
     def __obstacle_back(self):
         def on_avoiding_ended():
             self.__sensorBack.when_out_of_range = None
             self.move_idle(2)
-        self.__stop_avoiding()
+        self.__reset_avoidances()
         self.__log.info('Avoiding obstacle on back')
         self.__robot.forward(speed=self.__avoidance_speed, curve_left=0.5)
         self.__sensorBack.when_out_of_range = on_avoiding_ended
@@ -124,6 +134,5 @@ class Movement(FaceDetectorEventListener):
         self.__robot.right(speed=self.__standard_speed)
 
     def stop(self):
-        self.__stop_avoiding()
         self.__robot.stop()
 
