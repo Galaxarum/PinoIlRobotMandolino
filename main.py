31c290c1c0ca9fe92da5f007c6f9e86c2c1e7147
Galaxar <matteo.secco97@gmail.com>
2021-04-22 15:29:19 +0200

    Added nomovement argument to not instantiate movement

--- main.py
+++ main.py
@@ -34,15 +34,17 @@ def print_init_info():
 
 if __name__ == '__main__':
     # test_listener = Listener()
-    m = Movement(standard_speed=0.5, avoidance_speed=0.5)
     face_detector = FaceDetector(FILE_PATH, EXIT_CHAR, WAITING_INTERVAL, DEFAULT_CAMERA_DEVICE, CAM_RES_WIDTH,
                                  CAM_RES_HEIGHT, MIRROR_CAMERA)
+
+    if 'nomovement' not in sys.argv:
+        m = Movement(standard_speed=0.5, avoidance_speed=0.5)
+        face_detector.add_event_listener(m)
 
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
     print_init_info()
 
-    face_detector.add_event_listener(m)
     face_detector.start()
     face_detector.join()
 
