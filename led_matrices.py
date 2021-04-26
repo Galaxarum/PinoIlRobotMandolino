7e01d682e83c7c90f02a9a315a79a5143c8582d8
Galaxar <matteo.secco97@gmail.com>
2021-04-26 15:31:51 +0200

    Draft double mouth

--- led_matrices.py
+++ led_matrices.py
@@ -46,7 +46,7 @@ class LedMatrices:
 
         serial1 = spi(port=1, device=0, gpio=noop())
         self.mouth_matrix = max7219(serial1)
-        self.mouth = viewport(self.mouth_matrix, width=16, height=8)
+        self.mouth = viewport(self.mouth_matrix, width=24, height=8)
         self.enabled = True
 
         def scroll_mouth():
