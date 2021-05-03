b139c55452c35f6f233bd9ac1fb6b9da8a6ed261
Raspberry <matteo.secco97@gmail.com>
2021-05-03 18:11:40 +0200

    update game_inside to avoid repeat sensor trigger

--- game_inside.py
+++ game_inside.py
@@ -20,11 +20,16 @@ class GameMuseumDefinitive:
         # Phrases that the robot can play
         self.__random_phrases = ['I suggest you to visit all the rooms!', 'Look at these beautiful instruments!']
 
-        self.__threshold_distance = 0.05
+        self.__threshold_distance = 0.03
         # Confirm instrument
-        self.__sensorLeft = DistanceSensor(echo=17, trigger=23)
+        self.__sensorLeft = DistanceSensor(echo=17, trigger=23, queue_len=1, threshold_distance=0.03)
+        self.__sensorLeft.when_in_range = self.test_trigger
         # Change instrument
-        self.__sensorRight = DistanceSensor(echo=7, trigger=9)
+        self.__sensorRight = DistanceSensor(echo=7, trigger=9, queue_len=1, threshold_distance=0.03)
+        self.__sensorRight.when_in_range = self.test_trigger
+
+        self.left_distance = 1
+        self.right_distance = 1
 
         # (Not used alternative)
         # What to do when a foot is near the sensor: change instrument
@@ -32,6 +37,11 @@ class GameMuseumDefinitive:
 
         self.__tts = TTS()
         self.__emotion_controller = LedMatrices()
+
+    def test_trigger(self):
+        self.left_distance = self.__sensorLeft.distance
+        self.right_distance = self.__sensorRight.distance
+        #print('right triggered:', self.__sensorRight.distance)
 
     # (Eventually triggered by an event, but not in our case)
     def __change_instrument(self):
@@ -47,6 +57,7 @@ class GameMuseumDefinitive:
 
     def __say(self, text):
         self.__tts.say(text, blocking=True)
+        #print(text)
 
     def start_game(self):
         # Show a type of eye
@@ -59,24 +70,28 @@ class GameMuseumDefinitive:
         while True:
             # Wait for an answer by the user, checking manually thresholds
             # (while both aren't seeing obstacles do nothing)
-            left_distance = self.__sensorLeft.distance
-            right_distance = self.__sensorRight.distance
-            while left_distance > self.__threshold_distance and right_distance > self.__threshold_distance:
+            self.left_distance = 1
+            self.right_distance = 1
+            #print('Left distance:', left_distance)
+            #print('Right distance:', right_distance)
+            while self.left_distance > self.__threshold_distance and self.right_distance > self.__threshold_distance:
                 # Wait for 50 milliseconds
                 sleep(0.05)
                 # Reassign new values
-                left_distance = self.__sensorLeft.distance
-                right_distance = self.__sensorRight.distance
+                #left_distance = self.__sensorLeft.distance
+                #print('Left distance:', left_distance)
+                #right_distance = self.__sensorRight.distance
+                #print('Right distance:', right_distance)
 
             # As soon as an event occurs we check which of the two sensors has been triggered, checking saved values
 
             # Left (confirm) sensor activated
-            if left_distance <= self.__threshold_distance:
+            if self.left_distance <= self.__threshold_distance:
                 # Instrument is confirmed! We exit from the main loop and start to play sounds!
                 print('[Internal Game] Confirmed instrument')
                 break
             # Right (change inst.) sensor activated
-            elif right_distance <= self.__threshold_distance:
+            elif self.right_distance <= self.__threshold_distance:
                 print('[Internal Game] Change instrument')
                 self.__change_instrument()
             else:
